06081763c7057032fab6f7aeaac6332fd9df3909
weiichen2001 <weiichen2001@gmail.com>
2025-06-03 16:28:28 +0800

    圖片名稱修改+圖片陣列建立

---
+++ src/data/decoItems.js
@@ -0,0 +1,85 @@
+const decoItems = {
+
+    // 表情
+    facialExpression: [
+        { id: "f1", name: "face-01", src: "./images/decorate-items/facialExpression/face-01.png" },
+        { id: "f2", name: "face-02", src: "./images/decorate-items/facialExpression/face-02.png" },
+        { id: "f3", name: "face-03", src: "./images/decorate-items/facialExpression/face-03.png" },
+        { id: "f4", name: "face-04", src: "./images/decorate-items/facialExpression/face-04.png" },
+        { id: "f5", name: "face-05", src: "./images/decorate-items/facialExpression/face-05.png" },
+        { id: "f6", name: "face-06", src: "./images/decorate-items/facialExpression/face-06.png" },
+    ],
+
+    //   配件
+    accessories: [
+        { id: "a1", name: "accessory-01", src: "./images/decorate-items/accessories/accessory-01.png" },
+        { id: "a2", name: "accessory-02", src: "./images/decorate-items/accessories/accessory-02.png" },
+        { id: "a3", name: "accessory-03", src: "./images/decorate-items/accessories/accessory-03.png" },
+    ],
+
+    // 2025生日周邊
+    birthday: [
+        { id: "b1", name: "merch-01", src: "./images/decorate-items/birthday/merch-01.png" },
+        { id: "b2", name: "merch-02", src: "./images/decorate-items/birthday/merch-02.png" },
+        { id: "b3", name: "merch-03", src: "./images/decorate-items/birthday/merch-03.png" },
+        { id: "b4", name: "merch-04", src: "./images/decorate-items/birthday/merch-04.png" },
+        { id: "b5", name: "merch-05", src: "./images/decorate-items/birthday/merch-05.png" },
+        { id: "b6", name: "merch-06", src: "./images/decorate-items/birthday/merch-06.png" },
+    ],
+
+    // 專輯
+    albums: [
+        { id: "al1", name: "album-01", src: "./images/decorate-items/albums/album-01.jpg" },
+        { id: "al2", name: "album-02", src: "./images/decorate-items/albums/album-02.jpg" },
+        { id: "al3", name: "album-03", src: "./images/decorate-items/albums/album-03.jpg" },
+        { id: "al4", name: "album-04", src: "./images/decorate-items/albums/album-04.jpg" },
+        { id: "al5", name: "album-05", src: "./images/decorate-items/albums/album-05.jpg" },
+        { id: "al6", name: "album-06", src: "./images/decorate-items/albums/album-06.jpg" },
+        { id: "al7", name: "album-07", src: "./images/decorate-items/albums/album-07.jpg" },
+        { id: "al8", name: "album-08", src: "./images/decorate-items/albums/album-08.jpg" },
+        { id: "al9", name: "album-09", src: "./images/decorate-items/albums/album-09.jpg" },
+        { id: "al10", name: "album-10", src: "./images/decorate-items/albums/album-10.jpg" },
+        { id: "al11", name: "album-11", src: "./images/decorate-items/albums/album-11.jpg" },
+        { id: "al12", name: "album-12", src: "./images/decorate-items/albums/album-12.jpg" },
+        { id: "al13", name: "album-13", src: "./images/decorate-items/albums/album-13.jpg" },
+        { id: "al14", name: "album-14", src: "./images/decorate-items/albums/album-14.jpg" },
+        { id: "al15", name: "album-15", src: "./images/decorate-items/albums/album-15.jpg" },
+        { id: "al16", name: "album-16", src: "./images/decorate-items/albums/album-16.jpg" },
+        { id: "al17", name: "album-17", src: "./images/decorate-items/albums/album-17.jpg" },
+        { id: "al18", name: "album-18", src: "./images/decorate-items/albums/album-18.jpg" },
+        { id: "al19", name: "album-19", src: "./images/decorate-items/albums/album-19.jpg" },
+        { id: "al20", name: "album-20", src: "./images/decorate-items/albums/album-20.jpg" },
+    ],
+
+    // 海報
+    posters: [
+        { id: "p1", name: "HIT", src: "./images/decorate-items/posters/poster-01.jpg" },
+        { id: "p2", name: "sector17", src: "./images/decorate-items/posters/poster-02.jpg" },
+        { id: "p3", name: "17carat", src: "./images/decorate-items/posters/poster-03.jpg" },
+        { id: "p4", name: "goingSeventeen", src: "./images/decorate-items/posters/poster-04.jpg" },
+        { id: "p5", name: "falling-flower", src: "./images/decorate-items/posters/poster-05.jpg" },
+        { id: "p6", name: "ohMy", src: "./images/decorate-items/posters/poster-06.jpg" },
+        { id: "p7", name: "caratLand", src: "./images/decorate-items/posters/poster-07.jpg" },
+        { id: "p8", name: "odeToYou", src: "./images/decorate-items/posters/poster-08.jpg" },
+        { id: "p9", name: "secondWind01", src: "./images/decorate-items/posters/poster-09.jpg" },
+        { id: "p10", name: "secondWind02", src: "./images/decorate-items/posters/poster-10.jpg" },
+        { id: "p11", name: "teleparty01", src: "./images/decorate-items/posters/poster-11.jpg" },
+        { id: "p12", name: "follow", src: "./images/decorate-items/posters/poster-12.jpg" },
+        { id: "p13", name: "semicolon", src: "./images/decorate-items/posters/poster-13.jpg" },
+        { id: "p14", name: "secondWind03", src: "./images/decorate-items/posters/poster-14.jpg" },
+        { id: "p15", name: "teleparty02", src: "./images/decorate-items/posters/poster-15.jpg" },
+        { id: "p16", name: "attacca", src: "./images/decorate-items/posters/poster-16.jpg" },
+    ],
+
+      //  其他
+    others: [
+        { id: "o1", name: "lightstick-01", src: "./images/decorate-items/others/lightstick-01.png" },
+        { id: "o2", name: "lightstick-02", src: "./images/decorate-items/others/lightstick-02.png" },
+        { id: "o3", name: "lightstick-03", src: "./images/decorate-items/others/lightstick-03.png" },
+        { id: "o4", name: "lightstick-04", src: "./images/decorate-items/others/lightstick-04.png" },
+    ],
+
+
+
+
+}
