4372d649614f1a007255ce543ecbd70f437d75f8
weiichen2001 <weiichen2001@gmail.com>
2025-06-09 15:57:16 +0800

    hover-card大致完成

---
+++ src/data/merchItems.js
@@ -0,0 +1,181 @@
+const merchItems = [
+  // 壓克力立牌
+  {
+    id: "acstand",
+    img: "./images/merch/acstand.png",
+    alt: "HOSHI Acrylic Standee",
+    card: {
+      name: "2025 HOSHI Acrylic Standee - rawr ver.",
+      size: "W 7.5 × H 15.0 × D 0.3 cm",
+      material: "Acrylic",
+      desc: "HOSHI’s in full 10:10 mode, party hat on, tie slightly chaotic, and fingers ready to claw—but only if you forgot his birthday.",
+      price: "$8.50",
+      cardPosition: {
+        top: "21%",
+        left: "77%"
+      }
+    },
+    style: {
+      top: "25%",
+      left: "13%",
+      width: "25%",
+    }
+  },
+  //   photocard
+  {
+    id: "photocard",
+    img: "./images/merch/photocard.png",
+    alt: "Photocard Set",
+    card: {
+      name: "Photocard Set",
+      size: "5 pcs · 9x6 cm each",
+      material: "Coated Card",
+      desc: "A collection of iconic HOSHI moods to carry in your wallet.",
+      price: "$4.00",
+      cardPosition: {
+        top: "80%",
+        left: "-28%"
+      }
+    },
+    style: {
+      top: "20%",
+      left: "37%",
+      width: "14%",
+    }
+  },
+  //   postcard
+  {
+    id: "postcard",
+    img: "./images/merch/postcard.png",
+    alt: "postcard",
+    card: {
+      name: "2025 HOSHI Birthday Postcard - dumb ver.",
+      size: "10 × 14.8 cm (standard A6 size)",
+      material: "Paper",
+      desc: "He flopped like a cat. He smiled like a 🐯. He probably screamed “I’m a tiger!” before napping with his dog.",
+      price: "$2.00",
+      cardPosition: {
+        top: "80%",
+        left: "-88%"
+      }
+    },
+    style: {
+      top: "23%",
+      left: "54%",
+      width: "12%",
+    }
+  },
+  //   handfan
+  {
+    id: "handfan",
+    img: "./images/merch/handfan.png",
+    alt: "handfan",
+    card: {
+      name: "2025 HOSHI Birthday Hand Fan",
+      size: "W 20.0 cm × H 28.5 cm (including handle)",
+      material: "PVC fan sheet with plastic handle",
+      desc: "Need a breeze? Or just want to make sure HOSHI sees your claw pose from Row 98?This fan’s got you covered—literally and emotionally.",
+      price: "$5.50",
+      cardPosition: {
+        top: "80%",
+        left: "-71%"
+      }
+    },
+    style: {
+      top: "17%",
+      left: "68%",
+      width: "18%",
+    }
+  },
+  //   poster
+  {
+    id: "poster",
+    img: "./images/merch/poster.png",
+    alt: "poster",
+    card: {
+      name: "2025 HOSHI Birthday Poster – dumb ver",
+      size: "29.7 × 42.0 cm (A3)",
+      material: "Coated art paper",
+      desc: "He probably screamed “I’m a tiger!” before napping with his dog—again. Now printed bigger, for maximum emotional damage and wall impact.",
+      price: "$6.50",
+      cardPosition: {
+        top: "77%",
+        left: "-15%"
+      }
+    },
+    style: {
+      top: "46%",
+      left: "38%",
+      width: "25%",
+    }
+  },
+  //   badge1
+  {
+    id: "badge1",
+    img: "./images/merch/badge1.png",
+    alt: "badge1",
+    card: {
+      name: "2025 HOSHI Birthday Badge – punch ver.",
+      size: "Diameter: 5.8 cm",
+      material: "Tinplate",
+      desc: "You’ve seen “Tiger Punch HOSHI” on stage—now he fits in your palm (and on your bag). Strong pose, serious face, zero actual threat.",
+      price: "$2.50",
+      cardPosition: {
+        top: "61%",
+        left: "-87%"
+      }
+    },
+    style: {
+      top: "45%",
+      left: "66%",
+      width: "12%",
+    }
+  },
+  //   badge2
+  {
+    id: "badge2",
+    img: "./images/merch/badge2.png",
+    alt: "badge2",
+    card: {
+      name: "2025 HOSHI Birthday Badge – birthday ver.",
+      size: "Diameter: 5.8 cm",
+      material: "Tinplate",
+      desc: "Celebrate HOSHI’s birthday all year long—or at least every time you look at your bag. He's got the hat, the claws, and that look that says “it’s MY day.”",
+      price: "$2.50",
+      cardPosition: {
+        top: "61%",
+        left: "-80%"
+      }
+    },
+    style: {
+      top: "45%",
+      left: "75%",
+      width: "12%",
+    }
+  },
+  //   sticker-pack1
+  {
+    id: "sticker-pack1",
+    img: "./images/merch/sticker-pack1.png",
+    alt: "sticker-pack1",
+    card: {
+      name: "2025 HOSHI Sticker Pack - Weekend With Hoshi ver.",
+      size: "Each sticker approx. 4–6 cm wide",
+      material: "Water-resistant vinyl",
+      desc: "No schedule, just snacks, sunshine, and a suspiciously stylish dog. This sticker pack includes Just the basics: flowers, lunch, and one overdressed tiger.",
+      price: "$2.50",
+      cardPosition: {
+        top: "-197%",
+        left: "-93%"
+      }
+    },
+    style: {
+      top: "75%",
+      left: "40%",
+      width: "10%",
+    }
+  },
+
+];
+
+export default merchItems;
